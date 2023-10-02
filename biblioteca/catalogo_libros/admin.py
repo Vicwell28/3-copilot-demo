5ff5d74507d676cdbd2cd37f64a97c2645623a29
Merce-24 <139511155+Merce-24@users.noreply.github.com>
2023-10-02 22:22:29 +0000

    --Carga de modelo

--- biblioteca/catalogo_libros/admin.py
+++ biblioteca/catalogo_libros/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Libro
+admin.site.register(Libro)
 # Register your models here.
--- biblioteca/catalogo_libros/models.py
+++ biblioteca/catalogo_libros/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 # Create your models here.
+class Libro(models.Model):
+    titulo = models.CharField(max_length=255)
+    descripcion = models.CharField(max_length=255)
+    anio = models.IntegerField()
+    editorial = models.CharField(max_length=255)
+
+def __str__(self):
+        """
+        String que representa al objeto Book
+        """
+        return self.title
+
+def get_absolute_url(self):
+        """
+        Devuelve el URL a una instancia particular de Book
+        """
+        return reverse('book-detail', args=[str(self.id)])
