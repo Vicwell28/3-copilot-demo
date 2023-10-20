72b1ed26fee9d552357fa61f09dd802c554f06fd
Camily-24 <148476088+Camily-24@users.noreply.github.com>
2023-10-20 20:53:25 +0000

    CRUD de libros

--- biblioteca/catalogo_libros/urls.py
+++ biblioteca/catalogo_libros/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 
 urlpatterns = [
  path('', views.index, name='index'),
+ path('crear/', views.crear_libro, name='crear_libro'),
+ path('editar/<int:id>/', views.editar_libro, name='editar_libro'),
+ path('eliminar/<int:id>/', views.eliminar_libro, name='eliminar_libro'),
 ]
--- biblioteca/catalogo_libros/views.py
+++ biblioteca/catalogo_libros/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse, redirect
 from .models import Libro
 
 def index(request):
     libros = Libro.objects.all()
     return render(request, 'index.html', {'libros': libros})
+##Acciones del crud
+def crear_libro(request):
+    if request.method == 'POST':
+        titulo = request.POST['titulo']
+        descripcion = request.POST['descripcion']
+        editorial = request.POST['editorial']
+        anio = request.POST['anio']
+        libro = Libro(titulo=titulo, descripcion=descripcion, anio=anio,editorial=editorial)
+        libro.save()
+        return redirect('index')
+    else:
+        return render(request, 'crear.html')
+
+def editar_libro(request, id):
+    libro = Libro.objects.get(id=id)
+    if request.method == 'POST':
+        libro.titulo = request.POST['titulo']
+        libro.descripcion = request.POST['descripcion']
+        libro.anio = request.POST['anio']
+        libro.editorial = request.POST['editorial']
+        libro.save()
+        return redirect('index')
+    else:
+        return render(request, 'editar.html', {'libro': libro})
+
+def eliminar_libro(request, id):
+    libro = Libro.objects.get(id=id)
+    libro.delete()
+    return redirect('index')
